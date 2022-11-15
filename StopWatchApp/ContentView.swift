cae6fd007dba5b2e6106f1caa25e45f7361270f8
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2022-11-15 15:02:35 -0500

    Initial Commit

---
+++ StopWatchApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StopWatchApp
+//
+//  Created by Emanuel Gross von Trockau on 2022-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StopWatchApp/StopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchAppApp.swift
+//  StopWatchApp
+//
+//  Created by Emanuel Gross von Trockau on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
