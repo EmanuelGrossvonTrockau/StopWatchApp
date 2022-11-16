dc97b021c01d39223711ce6e52b027bc208bb278
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2022-11-16 11:33:38 -0500

    *

--- StopWatchApp/ContentView.swift
+++ StopWatchApp/ContentView.swift
@@ -12,12 +12,13 @@ struct ContentView: View {
         ZStack {
             
             Color.black
-        
+            
                 .ignoresSafeArea()
             
             
             
             VStack{
+                Spacer()
                 Text("00:09:96")
                     .font(Font.system(size:90, weight: .thin))
                     .foregroundColor(.white)
@@ -30,42 +31,84 @@ struct ContentView: View {
                     Spacer()
                     CircleBottonView(buttonColor: Color("Dark Green"), label: "Start", labelColor: .green)
                 }
+                List{
+                    
+                    Group{
+                        HStack {
+                            Text("Lap 5")
+                            Spacer()
+                            Text("00:00.98")
+                        }
+                        HStack{
+                            Text("Lap 4")
+                                .foregroundColor(.red)
+                            Spacer()
+                            Text("00:04:08")
+                                .foregroundColor(.red)
+                        }
+                        HStack{
+                            Text("Lap 3")
+                                .foregroundColor(.green)
+                            Spacer()
+                            Text("00:00:96")
+                                .foregroundColor(.green)
+                        }
+                        HStack{
+                            Text("Lap 2")
+                            Spacer()
+                            Text("00:02:76")
+                        
+                        }
+                        HStack{
+                            Text("Lap 1")
+                            Spacer()
+                            Text("00:01:16")
+                        }
+                    }
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                }
+                .frame(height: 300)
+                .listStyle(.plain)
                 }
             
-                .padding()
-            }
+            .padding()
         }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         
-        TabView{
-            
+        TabView(selection: Binding.constant(3)) {
+        
             Text("World Clock")
                 .tabItem{
                     Image(systemName: "globe")
                     Text("World Clock")
                 }
+                .tag(1)
+            
             Text("Alarm")
                 .tabItem{
                     Image(systemName: "alarm.fill")
                     Text("Alarm")
                     
                 }
+                .tag(2)
+            
             ContentView()
                 .tabItem{
                     Image(systemName: "stopwatch.fill")
                     Text("Stopwatch")
                 }
+                .tag(3)
+            
             Text("Timer")
                 .tabItem{
                     Image(systemName: "timer")
                     Text("Timer")
-                
-               
-            }
-                
+                }
+                .tag(4)
         }
         .accentColor(.orange)
         .preferredColorScheme(.dark)
